3d545679e88734de8a5f17f0541bdf891b00d961
PiotrWieczerzak78 <pjfw78@gmail.com>
2022-05-28 13:57:56 +0200

    change commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div>
     <h1>Twój email to : {{email}}</h1>
     <input type="email" v-model="email">
-    {{email.length}}
+    <p>Długość : {{email.length}}</p>
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else class="zadlugi">Twój adres e-mail jest stanowczo za długi.</div>
   </div>
 </template>
 
@@ -20,5 +23,7 @@ export default {
 </script>
 
 <style>
-
+.zadlugi{
+  color: red;
+}
 </style>
